c7700c7c4ccc47010a6020dfe9460b250eebdc27
jameshicklin <hickin.james@gmail.com>
2013-06-12 09:04:37 -0700

    Initial Commit

---
+++ FoodDiary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FoodDiary
+//
+//  Created by James Hicklin on 2013-06-12.
+//  Copyright (c) 2013 James Hicklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([FDAppDelegate class]));
+  }
+}
